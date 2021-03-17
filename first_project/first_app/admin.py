bcafcc94dd68a3b5927dd0ef2c327a61648d1231
Tapaj Kumar Das <tapajkumardas@gmail.com>
2021-03-17 23:28:16 +0530

    Fixes linting errors in python files

--- first_project/first_app/admin.py
+++ first_project/first_app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from first_app.models import Contact
+from .models import Contact
 
-# Register your models here.
 
 admin.site.register(Contact)
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from datetime import datetime
 from first_app.models import Contact
 from django.contrib import messages
-# Create your views here.
+
+
 def index(request):
     context = {
         'variable1': "its me Django Expert!",
         'variable2': "its me Python Expert!",
     }
-    # messages.success(request,"this is test messages")
-    return render(request,'index.html',context)
+    return render(request, 'index.html', context)
+
 
 def blog(request):
-    return render(request,'blog.html')
-    # return HttpResponse(" hey Django expert !!, you are in blog page")
+    return render(request, 'blog.html')
+
 
 def contact(request):
     if request.method == "POST":
@@ -21,11 +22,7 @@ def contact(request):
         phone = request.POST.get('phone')
         email = request.POST.get('email')
         desc = request.POST.get('desc')
-        contact = Contact(name=name,phone=phone,email=email,desc=desc,date=datetime.today())
+        contact = Contact(name=name, phone=phone, email=email, desc=desc, date=datetime.today())
         contact.save()
         messages.success(request, 'Your message has been sent!')
-    return render(request,'contact.html')
-    # return HttpResponse(" hey Django expert  , you are in  !!contact page")
-
-
-
+    return render(request, 'contact.html')
