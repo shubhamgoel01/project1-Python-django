49b728365208ae609cfd23f807e4cb22634968e6
Tapaj Kumar Das <tapajkumardas@gmail.com>
2021-03-18 01:18:11 +0530

    Adds sending email on contact form submit
    - Uses sendgrid to send emails
    - Updates .env file
    - Minor UI modifications
    - Updates hardcoded URLs

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -3,7 +3,7 @@ from .views import blog, contact, index
 
 
 urlpatterns = [
-    path('', index, name='Home'),
+    path('', index, name='home'),
     path('blog', blog, name='blog'),
     path('contact', contact, name='contact'),
 ]
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from datetime import datetime
 from first_app.models import Contact
 from django.contrib import messages
+from os import getenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 
 def index(request):
@@ -22,7 +25,25 @@ def contact(request):
         phone = request.POST.get('phone')
         email = request.POST.get('email')
         desc = request.POST.get('desc')
+
+        # Saving to database
         contact = Contact(name=name, phone=phone, email=email, desc=desc, date=datetime.today())
         contact.save()
+
+        # Sending mail
+        message = Mail(
+            from_email=str(getenv("SENDGRID_EMAIL_ID")),
+            to_emails="tapajkumardas@gmail.com",
+            subject="New contact request",
+            html_content='<strong>' + name + '<br>' + phone + '<br>' + email + '<br>' + desc + '<strong>')
+        try:
+            sg = SendGridAPIClient(getenv("SENDGRID_API_KEY"))
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e.message)
+
         messages.success(request, 'Your message has been sent!')
     return render(request, 'contact.html')
